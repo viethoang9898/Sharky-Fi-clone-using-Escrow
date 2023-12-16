c4df29e71ad3a1567120d95917860b342693bf15
viethoang9898 <153875716+viethoang9898@users.noreply.github.com>
2023-12-16 11:14:01 +0700

    declare program id

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("FUAG1wVSYU5AxnHqMu2QPvz7Qxe1CiARP15uBQ3DmgUf");
 
 #[program]
 pub mod nft_lend_borrow {
